d9bc0c1d09eacda6a48f1e9cbf52509e497c311d
valery <valery.fremaux@gmail.com>
2016-09-25 23:06:52 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -17,10 +17,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * @package   block_shop_bills
- * @category  blocks
- * @author    Valery Fremaux (valery.fremaux@gmail.com)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Version details.
+ *
+ * @package     block_shop_bills
+ * @category    blocks
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2016 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 $plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX)
@@ -28,3 +31,6 @@ $plugin->requires  = 2014110400;        // Requires this Moodle version
 $plugin->component = 'block_shop_bills'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC; // Full name of the plugin (used for diagnostics)
 $plugin->release = '2.8.0 (Build 2016022500)'; // Full name of the plugin (used for diagnostics)
+
+// Non moodle attributes.
+$plugin->codeincrement = '2.8.0000';
