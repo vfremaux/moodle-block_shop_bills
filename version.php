4e1a41b0ba915e9f02b86bac3ec8ab763a52850e
valery <valery.fremaux@gmail.com>
2016-09-26 09:46:34 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Version details.
  *
@@ -26,11 +24,13 @@ defined('MOODLE_INTERNAL') || die();
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014110400;        // Requires this Moodle version
-$plugin->component = 'block_shop_bills'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_RC; // Full name of the plugin (used for diagnostics)
-$plugin->release = '2.8.0 (Build 2016022500)'; // Full name of the plugin (used for diagnostics)
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014110400;        // Requires this Moodle version.
+$plugin->component = 'block_shop_bills'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '2.8.0 (Build 2016022500)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '2.8.0000';
