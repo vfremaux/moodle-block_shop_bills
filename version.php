b3102bd80a470e85e118b49a455592b3fd90f984
valery <valery.fremaux@gmail.com>
2017-06-18 18:28:26 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014110400;        // Requires this Moodle version.
+$plugin->requires  = 2016120500;        // Requires this Moodle version.
 $plugin->component = 'block_shop_bills'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.8.0 (Build 2016022500)';
+$plugin->release = '3.2.0 (Build 2016022500)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.8.0000';
+$plugin->codeincrement = '3.2.0000';
