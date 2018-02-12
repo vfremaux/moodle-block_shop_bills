49ed91b538b6837bb998c6e828abb4b3db5d07d3
valery <valery.fremaux@gmail.com>
2018-02-12 14:11:00 +0100

    Resync new versoins

--- lang/en/block_shop_bills.php
+++ lang/en/block_shop_bills.php
@@ -21,8 +21,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['shop_bills:addinstance'] = 'Can add an instance';
-$string['shop_bills:myaddinstance'] = 'Can add an instance to My Page';
+$string['shop_bills:addinstance'] = 'Add an instance';
+$string['shop_bills:myaddinstance'] = 'Add an instance to My Page';
 
 $string['blockname'] = 'My transactions';
 $string['title'] = 'Title';
@@ -31,5 +31,5 @@ $string['configtitle'] = 'Title';
 $string['leaveblanktohide'] = 'Leave blank to hide';
 $string['nobills'] = 'No bills in your account';
 $string['noshops'] = 'No shops';
-$string['notconfigured'] = 'This block is not configured. A courseshop instance MUST be assigned.';
+$string['notconfigured'] = 'This block is not configured. A course shop instance MUST be assigned.';
 $string['pluginname'] = 'My bills';
