16e5273801e7c66e85ced9a3343c2c4637e39317
Valery Fremaux <valery.fremaux@gmail.com>
2021-04-02 10:46:20 +0200

    Automated sync from SVN

--- lang/fr/block_shop_bills.php
+++ lang/fr/block_shop_bills.php
@@ -34,5 +34,5 @@ $string['leaveblanktohide'] = 'Laisser vide pour cacher le titre';
 $string['nobills'] = 'Aucune facture à votre nom';
 $string['noshops'] = 'Pas de boutiques dans ce site';
 $string['notconfigured'] = 'Ce bloc n\'est pas configuré. Une instance de boutique doit être désignée.';
-$string['pluginname'] = 'Mes factures';
+$string['pluginname'] = 'Boutique : Mes factures';
 $string['title'] = 'Titre';
