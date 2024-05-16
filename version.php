55ae04240a9400dae627fc1382d7d1eda383a043
Valery Fremaux <valery.fremaux@gmail.com>
2024-05-16 08:57:51 +0200

    Push up 4.1 version

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020060900;        // Requires this Moodle version.
+$plugin->requires  = 2022112801;        // Requires this Moodle version.
 $plugin->component = 'block_shop_bills'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.9.0 (Build 2016022500)';
-$plugin->supported = [39, 311];
+$plugin->release = '4.1.0 (Build 2016022500)';
+$plugin->supported = [401, 402];
 $plugin->dependencies = ['local_shop' => 2022090900];
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.9.0000';
+$plugin->codeincrement = '4.1.0000';
